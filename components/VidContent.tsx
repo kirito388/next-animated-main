b33bd569eea81a3efb12c889ca6aa5be7d1d3b8f
kirito388 <bell771177@gmail.com>
2024-11-07 02:40:25 -0800

    Initial commit

---
+++ components/VidContent.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const VidContent = () => {
+  return (
+    <div className="w-[90%] p-10 z-[20] flex items-center">
+      <div className="w-full flex flex-col md:flex-row gap-20 mt-24 items-center">
+        <div className="flex flex-col gap-6 mb-6">
+          <h1 className="text-6xl text-[#FFD700]">My CV</h1>
+          <p className="text-lg max-w-[600px] text-gray-400">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam
+          </p>
+        </div>
+        <iframe
+          width="590"
+          height="350"
+          src=""
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; fullscreen; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+          className="rounded-lg w-[450px] md:w-[590px] mr-6"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default VidContent;
---
+++ constants/index.ts
@@ -0,0 +1,70 @@
+export const Skill_data = [
+  {
+    skill_name: "Html 5",
+    Image: "/html.png",
+  },
+  {
+    skill_name: "Css",
+    Image: "/css.png",
+  },
+  {
+    skill_name: "Java Script",
+    Image: "/js.png",
+  },
+  {
+    skill_name: "Tailwind Css",
+    Image: "/tailwind.png",
+  },
+  {
+    skill_name: "React",
+    Image: "/react.png",
+  },
+];
+
+export const Socials = [
+  {
+    name: "Instagram",
+    src: "/instagram.svg",
+  },
+  {
+    name: "Youtube",
+    src: "/youtube.svg",
+  },
+  {
+    name: "Discord",
+    src: "/discord.svg",
+  },
+];
+
+export const Projects = [
+  {
+    name: "Portfolio",
+    src: "/starwars.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    name: "Space",
+    src: "/SpaceWebsite.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    name: "Podcast",
+    src: "/matrix.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    name: "Component",
+    src: "/skills.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    name: "Lend.mn",
+    src: "/webapp.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
